4cc72a542256a3e7a3e78bb52d402c2dd64ac442
jihoon_gweon <kjh8673a@naver.com>
2023-04-24 20:47:34 +0900

    [Add] tailwindcss 설정파일 추가 / 적용

--- front/src/App.js
+++ front/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88f82694ae34905374afc97ed5d88fcca323d0087e8018af20d1da7bc79bbaed
-size 1415
+oid sha256:dd083f68baf45ca51ae40f66c27960f1481ebf8d719d44b56c0b9d4ef7d6e995
+size 1519
