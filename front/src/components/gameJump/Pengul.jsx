5a7686e2375876d078b4655d2f89848367b0c736
its19447@gmail.com <its19447@gmail.com>
2023-05-01 16:13:07 +0900

    [Fix] 백그라운드 색 사라짐 연구 제곧내

--- front/src/components/gameJump/Pengul.jsx
+++ front/src/components/gameJump/Pengul.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85a9356389473809db79cf5b71cfaa20a7ac2b6bc6e40fa7e1ec559e75043060
-size 5169
+oid sha256:9f598bb50c13597eec2ba32244bcd4be90bbf17e11d318b734046d8c59416d91
+size 5226
