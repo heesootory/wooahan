07c77bee69ba9cf0002002e4698af2a38949c3e6
조원희 <penguin1998@daum.net>
2023-05-01 21:12:43 +0900

    [Feat] 튜토리얼 페이지 구현 사이드바로 게임마다 해당 컴포넌트가 랜더링될 수 있도록 구성

--- front/src/components/common/GameEnding.jsx
+++ front/src/components/common/GameEnding.jsx
@@ -1,3 +1,32 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:891249d947164bef75ee7fcd507ea25da4632a7f7b611bc5d313885f091bbbdf
-size 791
+// import PropTypes from "prop-types";
+import React from "react";
+import { connect } from "react-redux";
+
+import playground from "assets/images/background_playground.jpg";
+
+export const GameEnding = (props) => {
+  return (
+    <div
+      className="grid items-center w-screen h-screen"
+      style={{
+        background: `url(${playground})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="text-[#6937A1] font-MaplestoryBold text-6xl text-stroke-mainWhite text-stroke-2">
+        야호!
+      </div>
+    </div>
+  );
+};
+
+GameEnding.propTypes = {
+  //   second: PropTypes.third,
+};
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GameEnding);
--- front/src/components/gameBubble/AnswerCard.jsx
+++ front/src/components/gameBubble/AnswerCard.jsx
@@ -1,3 +1,43 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:09fb36d74460c24c5b9f8fad7eb4e4a45e4c46077608e1018f4790d1d516e050
-size 1174
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+
+export const AnswerCard = (props) => {
+  useEffect(() => {
+    if (props.name === props.answer) {
+      setTimeout(() => {
+        props.closeCard();
+        props.changeQuiz();
+      }, 1500);
+    } else {
+      setTimeout(() => props.closeCard(), 1500);
+    }
+  }, [props]);
+
+  return (
+    <div className="w-screen h-screen">
+      <div className="absolute grid -mt-40 -ml-40 overflow-hidden border-4 border-mainBlack bg-mainWhite justify-items-center h-80 rounded-3xl top-1/2 left-1/2 w-80">
+        <div
+          className="h-64 overflow-hidden w-80"
+          style={{
+            backgroundImage: `url(${props.image})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        ></div>
+        <div className="h-16 text-5xl font-bold leading-16 font-netmarbleB">
+          {props.name}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+AnswerCard.propTypes = {
+  //   second: PropTypes.third
+};
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AnswerCard);
