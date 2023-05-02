31165836125ba55ca74785d336fa717092d3242d
신현광 <rhkd0363@gmail.com>
2023-05-02 12:48:30 +0900

    [Add] Sleigh Slice 추가

--- front/src/store/store.js
+++ front/src/store/store.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f71b985c7e08176aff9a85aed592350da75a417d6a65e4d1e55d51140973b7d2
-size 203
+oid sha256:148240a76cd441ab6fe98206ee5fd80355d7dbef4f48d293f2b93a96379d8598
+size 289
