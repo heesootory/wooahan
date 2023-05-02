91b0da471e3fd10d146a56be16fb2c1d3ad93a5d
Jihoon <kjh8673a@naver.com>
2023-05-02 13:43:43 +0900

    [Add] 방울 애니메이션, 보상 스타 애니메이션

--- front/src/components/gameBubble/WordBubble.jsx
+++ front/src/components/gameBubble/WordBubble.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86e4000401e38f214f7e1347f8c00ca56bdbeaddb9ede11fa6d8b2d9833b902b
-size 1216
+oid sha256:772f67b6b38ba1445d2aa435dbc9d012b9442043831da3ee06b95ce86c9537be
+size 1460
