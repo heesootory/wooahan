fe1fcf2e69629d85289ed7818e1296258d72e0dc
itss <its19447@gmail.com>
2023-05-03 10:00:52 +0900

    [Add] 테스트용 타이머 추가 근데 원래 이렇게 많이 바뀌는 거임?

--- hangul/app/src/main/java/com/penguin/hangul/MainActivity.kt
+++ hangul/app/src/main/java/com/penguin/hangul/MainActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1558d0858f18ed8fec070e5d13e4c0b13ea2b8d12dc4b03dd49a0f624f36bfac
-size 1409
+oid sha256:8ad211f8087db427ba7baa6d8f7ec0ce1c88b7a7f777c3c0ba7d0cc8096aa6b8
+size 3574
