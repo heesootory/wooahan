e2c11719de5243b07ff5274d199c89e8e338463f
jihoon_gweon <kjh8673a@naver.com>
2023-05-04 15:16:29 +0900

    [Fix] 통신에 맞게 변수명 수정

--- front/src/pages/GameBubble.jsx
+++ front/src/pages/GameBubble.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afa4c609794004b85de1a06365766d23c4f44b8815b75d19dbe531df77f529d9
-size 5924
+oid sha256:a18d48b21c1d1f593851ae5c45549fe77c881e0048ddf6f7b8fcecc010351db7
+size 5973
