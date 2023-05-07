bcf5bf6fe78db49635d43de635bdb2a94f592108
jiwoo lee <its19447@gmail.com>
2023-05-07 19:30:18 +0900

    [Add] 테스트 코드 넣기 전 커밋하기 ㅈㄱㅇ

--- front/src/pages/GameJump.jsx
+++ front/src/pages/GameJump.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26404293a983298042ae67f720354f3c0efafeb31c987848253e80696f0f2bdc
-size 4371
+oid sha256:7827ca97b06b717a89583a85df2a1686d04221f9375a4fba5b274dd87d95ab5d
+size 4699
