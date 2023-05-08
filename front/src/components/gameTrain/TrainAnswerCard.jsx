3af31d61b70b2d468078f61e976b460b59619563
조원희 <penguin1998@daum.net>
2023-05-08 17:57:59 +0900

    머지

--- front/src/components/gameTrain/TrainAnswerCard.jsx
+++ front/src/components/gameTrain/TrainAnswerCard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1964030682f9d4c0bc96d70286fb86ef8bea87a51efca8b5deb7b70b01c7e1b
-size 2030
+oid sha256:a0305d732e074dc2a0a3c5a64efa4876c38aadf241f3c8731e0f2cd0df7bc2a1
+size 2025
