c16e6061a8067719d7d7d7908e745fd436f1e5a5
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-05-08 13:11:56 +0900

    [feat] get android info
    Former-commit-id: 29c24085b6896a3c72c573e0e73539b59319071b

--- hangul/app/src/main/java/com/penguin/hangul/MainActivity.kt
+++ hangul/app/src/main/java/com/penguin/hangul/MainActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d061ec622f001ca8fde9c170b7ec838c254a32be50724bc03fa7b9fc37a4bb26
-size 5208
+oid sha256:0bf1613ece2183a827508d6bf766d8c935231ba86a994758b5cb6951bdcc8324
+size 6013
