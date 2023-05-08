8ce81c5b4bba8db4d5fd54c9e5488d0872b8d4bc
신현광 <rhkd0363@gmail.com>
2023-05-08 11:19:31 +0900

    [Add] Sensor 추ã가

--- hangul/app/src/main/java/com/penguin/hangul/MainActivity.kt
+++ hangul/app/src/main/java/com/penguin/hangul/MainActivity.kt
@@ -1,3 +1,120 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:feddb953515607b4179fb7ca0e906396c5bcc81ee4bdf4bd55bfd5a964fdbc55
-size 978
+package com.penguin.hangul
+
+import android.app.Activity
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.hardware.*
+import android.os.Bundle
+import android.os.Debug
+import android.util.Log
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.sqrt
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var myWebView: WebView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+
+        myWebView = findViewById(R.id.myWebView)
+
+        myWebView?.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+        }
+
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager;
+        val accelermeter = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        val gravity = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+
+        val sensorEventListner = object : SensorEventListener {
+            var lastAccel: Float = 0f;
+            var accel: Float = 0f;
+            var currentAccel = 0f;
+
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+            }
+
+            override fun onSensorChanged(event: SensorEvent) {
+                if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                    lastAccel = currentAccel
+                    currentAccel =
+                        sqrt((event.values[0] * event.values[0] + event.values[1] * event.values[1] + event.values[2] * event.values[2]))
+                    val delta: Float = currentAccel - lastAccel
+                    accel = accel * 0.9f + delta;
+
+//                    Log.i("Android", "this is ACCELEROMETER")
+                    if (accel > 2) {
+                        onJumpDetected()
+                    }
+                }
+
+                if (event.sensor.type == Sensor.TYPE_GRAVITY) {
+                    val y = event.values[1]
+
+                    if (y > 2) {
+                        onMoveDetected(1)
+                    } else if (y < -2) {
+                        onMoveDetected(-1)
+                    } else {
+                        onMoveDetected(0)
+                    }
+                }
+            }
+        }
+
+        myWebView?.addJavascriptInterface(MySensorManager(sensorManager, sensorEventListner, gravity), "sleigh")
+        myWebView?.loadUrl("http://10.0.2.2:3000")
+    }
+
+    fun onJumpDetected() {
+        runOnUiThread {
+            myWebView?.evaluateJavascript("javascript:window.doJump();", null)
+        }
+    }
+
+    fun onMoveDetected(value: Int) {
+        if (value == 0) {
+            runOnUiThread {
+                myWebView?.evaluateJavascript("javascript:window.stopMove()", null)
+            }
+        } else {
+            runOnUiThread {
+                myWebView?.evaluateJavascript("javascript:window.doMove(${value})", null)
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (myWebView?.canGoBack() == true) {
+            myWebView?.goBack()
+        } else {
+            finish()
+        }
+    }
+}
+
+class MySensorManager(private val sensorManager: SensorManager,private val sensorEventListener: SensorEventListener,private val sensor:Sensor){
+    @JavascriptInterface
+    fun resumeSensor(){
+        sensorManager.registerListener(
+            sensorEventListener,
+            sensor,
+            SensorManager.SENSOR_DELAY_NORMAL
+        );
+    }
+    @JavascriptInterface
+    fun pauseSensor(){
+        sensorManager.unregisterListener(sensorEventListener,sensor);
+    }
+}
