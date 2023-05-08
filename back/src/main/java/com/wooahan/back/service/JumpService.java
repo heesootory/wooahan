ac6862302733daac14b5ae26c35fa23d5c5780cf
이상민 <lsms3723@daum.net>
2023-05-08 17:38:40 +0900

    [FIX] dto 데이터바인딩 오류

--- back/src/main/java/com/wooahan/back/service/JumpService.java
+++ back/src/main/java/com/wooahan/back/service/JumpService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f6d4151fce3e6cb718bf3be1886cec5e8b4add11def638f5f11548ff748ef52
-size 2148
+oid sha256:7d6900140dca27faa784880e47d2c9a34793867d9326ec1888bb43e30b931a78
+size 2105
--- back/src/main/java/com/wooahan/back/service/RunService.java
+++ back/src/main/java/com/wooahan/back/service/RunService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba13eca7bed5757f88e099438e23689e10b0866b377bacaef91b350dd6b0c93e
-size 1883
+oid sha256:dc72fae85ea9925eb7c42fe8571afe46cfd51e3a9507b82d438aa2f315785df6
+size 1917
