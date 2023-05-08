0a5891899f87025dd81c01f2c6314172d5a0ccba
신현광 <rhkd0363@gmail.com>
2023-05-08 09:07:27 +0900

    [File] 로딩페이지 사진 추가

--- front/src/components/gameTrain/TrainEnd.jsx
+++ front/src/components/gameTrain/TrainEnd.jsx
@@ -1,3 +1,17 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:207e23dd5e92e27dc43db2a0e7eb1594412eeefddd904d8ebdce0c5cdadc4d2c
-size 413
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+
+export const TrainEnd = (props) => {
+  return <div className="text-6xl">끝끝끝</div>;
+};
+
+TrainEnd.propTypes = {
+  //   second: PropTypes.third
+};
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TrainEnd);
