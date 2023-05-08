619f76adcd547a2019bcf2058073decb931bcb71
이상민 <lsms3723@daum.net>
2023-05-08 10:47:16 +0900

    [FIX] nginx

--- back/src/main/java/com/wooahan/back/controller/GameController.java
+++ back/src/main/java/com/wooahan/back/controller/GameController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b6d1c515041f8ffa986fc367748aa5c766f0b5efedbd335811c4c4b8115ba63
-size 1566
+oid sha256:2a7096b21e9a40fa17fdbe3b0d7a91e8b6ba84d0f88500613e5dd46ae1ecb55d
+size 1570
--- back/src/main/java/com/wooahan/back/service/BubbleService.java
+++ back/src/main/java/com/wooahan/back/service/BubbleService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34e1874659b94bd35dd3731f09994fd9dc785912f0b3affca748617db65f5553
-size 1474
+oid sha256:12e0560476294b7c5b46c1733bd20f804bafc7864102d1070e3619db5dd8cf7f
+size 1585
