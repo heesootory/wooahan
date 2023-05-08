d9b10ee53983a1f48e227e2c7bd2a134194a5227
신현광 <rhkd0363@gmail.com>
2023-05-08 09:15:38 +0900

    [Style] CSS 추가

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74c97acaf11b9fb2ad1317eb7ad88cb04761a75beeea8a929d81f2a511d8b61a
-size 5560
+oid sha256:ec6da3631133105e6c139a4124e75c00d692630299ed6addafa845a83dbbe1fc
+size 7330
