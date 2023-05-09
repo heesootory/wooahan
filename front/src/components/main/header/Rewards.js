dab654784cb77c4fa650771f4c40a40f21321810
신현광 <rhkd0363@gmail.com>
2023-05-10 00:27:46 +0900

    [File] 여우 모델 추가

--- front/src/components/main/header/Rewards.js
+++ front/src/components/main/header/Rewards.js
@@ -1,3 +1,21 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5a52f5c00c7e6c012fecde478d433a0a4fbcc616a1d2c24fffaec16da6ca9f06
-size 694
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookOpenReader } from '@fortawesome/free-solid-svg-icons';
+import {useNavigate} from 'react-router-dom';
+
+library.add(faBookOpenReader);
+
+const Reward = () => {
+
+    const nav = useNavigate();
+
+    return (
+        <div onClick={() => {nav(`/books`)}} className="pl-4 w-1/6 h-full flex justify-start items-center">
+            <button className="bg-palePupple rounded-xl w-1/2 h-4/5 flex items-center justify-center">
+                <FontAwesomeIcon icon={faBookOpenReader} size='2xl'/>
+            </button>
+        </div>
+    )
+}
+
+export default Reward;
