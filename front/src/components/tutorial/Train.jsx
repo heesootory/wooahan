3f93cb52c017f0201937d6ea23027e907bd82eb0
조원희 <penguin1998@daum.net>
2023-05-09 17:57:56 +0900

    [Feat] 기차 튜토리얼 디자인 결정

--- front/src/components/tutorial/Train.jsx
+++ front/src/components/tutorial/Train.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cd56c1f72607917dac0288eb1de8fe97e0845936eb0c0ee5137c03db7b54578
-size 1293
+oid sha256:13c00c4ef8c3e395d9ebb96021dc1de90c4fee55cf8500aec7f0aa6a4fb84793
+size 1343
