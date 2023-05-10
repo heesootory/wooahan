4ba75494ec30304db5203f13975e08b1001477c8
신현광 <rhkd0363@gmail.com>
2023-05-10 23:46:39 +0900

    [Feat] 로딩화면 변경

--- front/src/components/gameSleigh/QuizResult.jsx
+++ front/src/components/gameSleigh/QuizResult.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea5bd475cd5bd383596ece2a65c6bf1962c57488af17056a4b78ef776921f91a
-size 2122
+oid sha256:a6cd683999ed279d79ee5ad241eacd289f9727788780b4644bf53c3f4d6186d3
+size 2129
