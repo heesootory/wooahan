90e340e68476f9e1af8ab39c42a351f6b65db00f
신현광 <rhkd0363@gmail.com>
2023-05-10 13:03:30 +0900

    [Add] game API 파일 추가

---
+++ front/src/api/game.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3666aeaf22f6b9b0035a8e474d9968537f0ef06234f39b2d451357924044e9f5
+size 763
--- front/src/components/main/modal/ModalHeader.js
+++ front/src/components/main/modal/ModalHeader.js
@@ -1,3 +1,29 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f56f0f906e83a32f9ad1f6c1b15cef586c8cc54bdb9661f47bf94bfe2265ac44
-size 852
+import { useDispatch } from "react-redux"
+import { modalActions } from 'store/features/mainCard/modalSlice'
+
+const ModalHeader = () => {
+
+    const dispatch = useDispatch();
+
+    const modalCloseHandler = () => {
+        dispatch(modalActions.closeModal())
+    }
+
+    return (
+        <header className="flex border-b border-black py-[2%]">
+            <div className="w-1/3"></div>
+            <div className="w-1/3 font-['MaplestoryOTFBold'] text-2xl">
+                설정
+            </div>
+            <div className="flex w-1/3 justify-end">
+                <button 
+                    onClick={modalCloseHandler}
+                    className="border border-black rounded-lg w-[20%] font-['MaplestoryOTFBold'] text-2xl">
+                    X
+                </button>
+            </div>
+        </header>
+    )
+}
+
+export default ModalHeader;
