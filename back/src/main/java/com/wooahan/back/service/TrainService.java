524efb29b71880f55e85ada05d29b514e1e7ba68
이상민 <lsms3723@daum.net>
2023-05-10 17:04:41 +0900

    [fix]random train

--- back/src/main/java/com/wooahan/back/service/TrainService.java
+++ back/src/main/java/com/wooahan/back/service/TrainService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9914f639555af351dbdbb3b8a9f388620b98ef4560e7385c2913aa0a226ef3b3
-size 1995
+oid sha256:fdc0353bcc2371f2af6e5f507d9dee8836055fb77470b18d484d7b3829fd56b8
+size 2052
