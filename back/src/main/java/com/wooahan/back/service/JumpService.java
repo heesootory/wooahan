e00e217a62bbab5bd478f289649798780ede68b0
이상민 <lsms3723@daum.net>
2023-05-10 14:15:07 +0900

    [fix]file name

--- back/src/main/java/com/wooahan/back/service/JumpService.java
+++ back/src/main/java/com/wooahan/back/service/JumpService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22ec2e1c3d1a290c77b638e8d0b8f42f8e6803e13c7b486cab6b58785f89a968
-size 2272
+oid sha256:a85491b599f2c2831c19f8f588698e164ac108f2bbbefa53a01fe656c57e0e56
+size 2274
