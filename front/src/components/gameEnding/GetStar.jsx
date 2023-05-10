72989391b50226b30deaede9f51db40edec106cb
신현광 <rhkd0363@gmail.com>
2023-05-10 17:02:33 +0900

    [Style] 스타일수정

--- front/src/components/gameEnding/GetStar.jsx
+++ front/src/components/gameEnding/GetStar.jsx
@@ -1,3 +1,48 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:133e9a615182d8b167a191eedc7895c14c0aa21e2d4073cd061e4e7966376bcc
-size 1335
+import RewardStar from "components/gameEnding/RewardStar";
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+
+export const GetStar = (props) => {
+  const [starCount, setStarCount] = useState(0);
+  const [starLoop, setStarLoop] = useState([]);
+
+  useEffect(() => {
+    if (props.starCount === 0) {
+      setStarCount(5);
+    } else {
+      setStarCount(props.starCount);
+    }
+    setTimeout(() => props.closeGetStar(), 2000);
+  }, [props]);
+
+  useEffect(() => {
+    const newArr = [];
+    for (let i = 0; i < 5; i++) {
+      if (i < starCount) {
+        newArr.push(<RewardStar key={i} color={i + 1} count={starCount} />);
+      } else {
+        newArr.push(<RewardStar key={i} color={0} count={starCount} />);
+      }
+    }
+    setStarLoop(newArr);
+  }, [starCount]);
+
+  return (
+    <>
+      <div className="relative w-screen h-screen bg-opacity-40 bg-mainGray-300"></div>
+      <div className="bg-[#8977AD] w-[24rem] h-[4.5rem] absolute top-2/3 left-1/2 -translate-x-[12rem] rounded-3xl grid grid-cols-5 items-center justify-items-center">
+        {starLoop}
+      </div>
+    </>
+  );
+};
+
+GetStar.propTypes = {
+  //   second: PropTypes.third,
+};
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GetStar);
--- front/src/components/gameSleigh/QuizResult.jsx
+++ front/src/components/gameSleigh/QuizResult.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93e65052cc53069beec4b305c9620303e3ce668b224ae3a7b02c02821c4ff269
-size 2111
+oid sha256:ea5bd475cd5bd383596ece2a65c6bf1962c57488af17056a4b78ef776921f91a
+size 2122
