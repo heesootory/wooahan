9485b88ad4404ba1a66685f31a40d2d18e06445c
jiwoo lee <its19447@gmail.com>
2023-05-10 23:37:59 +0900

    [Fix] 에러 픽스 ㅈㄱㄴ

--- front/src/components/main/loading/MainLoadingComponent.js
+++ front/src/components/main/loading/MainLoadingComponent.js
@@ -1,14 +1,3 @@
-
-
-const MainLoadingComponent = () => {
-
-    return (
-        <>
-            <div className="bg-sharkGray text-yellow h-full w-full">
-                Loading....
-            </div>
-        </>
-    )
-}
-
-export default MainLoadingComponent
+version https://git-lfs.github.com/spec/v1
+oid sha256:ffb8e1f6c62096186577a96d80d6a61ed3b9527bd3b2391c56a695ad1ec645d1
+size 236
--- front/src/util/hooks/usePreloadImages.jsx
+++ front/src/util/hooks/usePreloadImages.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fff0dd734d7960fc1bdcad3372925e72d15cb880ef2a3c7d22041acaadccc515
-size 780
+oid sha256:1a6c71fed7803774e339f4658f463ecd236b055f947db5c0f790492301c9d60a
+size 809
