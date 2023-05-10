004a2290e18b719361543eff753b99eda16ff136
신현광 <rhkd0363@gmail.com>
2023-05-11 00:27:54 +0900

    [Fix] 에러

--- front/src/pages/GameSleigh.jsx
+++ front/src/pages/GameSleigh.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3c968f877ea386dd200969b7a88f85642f90616a6a074a9108cfa8a3d9c3007
+oid sha256:97b60eb99fb5d64626933c67135f10cd636756cf5bacd314a223bcfcb4dc596b
 size 11560
