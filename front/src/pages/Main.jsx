0a67637e3f2cff3e6d3b7394bb070613a883b765
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-05-12 15:22:22 +0900

    [feat] get login cookie

--- front/src/pages/Main.jsx
+++ front/src/pages/Main.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2ca6152958a83c50cd01135b39d9c425fe0e394bfae74c45ab2528c0da06ad4
-size 1803
+oid sha256:75d8db5a7cea9a81915da7d7d2dcc46d50675d14f94bca2c94be3aecdc94ca1e
+size 1854
