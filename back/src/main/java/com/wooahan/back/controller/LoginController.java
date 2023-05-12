f9e6464dc5fd9a16efcc907e9928097e3c35418c
이상민 <lsms3723@daum.net>
2023-05-12 12:36:23 +0900

    [Add]login cookie

--- back/src/main/java/com/wooahan/back/controller/LoginController.java
+++ back/src/main/java/com/wooahan/back/controller/LoginController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8ddb41c9d4ab66147de179c5bc8210d6371596259cfa62202d482ccbe9ee703
-size 2462
+oid sha256:299c9d2df555510ffc72814f0a31121c39d67e8a24ab238371f94d3b29095e4b
+size 3030
--- back/src/main/java/com/wooahan/back/service/LoginService.java
+++ back/src/main/java/com/wooahan/back/service/LoginService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d00a94e25936ceb128b904e8eae2f07fb9b4c86ebfe8d0f994b8758789341ab
-size 4627
+oid sha256:c12c59f688b1d1c425f97e9788567633f053bc2e9834446e592d1bdecb26bc50
+size 5137
