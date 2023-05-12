55637ea41b0e31dbb09da0cd6f0ef6c4682591dd
이상민 <lsms3723@daum.net>
2023-05-12 12:37:00 +0900

    [test]

--- front/src/pages/GameSleigh.jsx
+++ front/src/pages/GameSleigh.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e192fb32d68f9dd5383336db09d285f543e9de4bc0bc75ad56be10714b01d89d
-size 14116
+oid sha256:9bea6af1b81dae10d1d8e6f139dc90332c8e62774b87ef481839e3831ccf2b09
+size 14115
