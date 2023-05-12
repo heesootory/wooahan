ad602e82faa01b91306fd8e8c066c21166db5d5c
조원희 <penguin1998@daum.net>
2023-05-12 10:00:27 +0900

    [Feat] 메인 로딩페이지 구현 완료

--- front/src/components/main/header/Setting.js
+++ front/src/components/main/header/Setting.js
@@ -1,26 +1,3 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGear } from '@fortawesome/free-solid-svg-icons';
-import { useDispatch } from "react-redux"
-import { modalActions } from 'store/features/mainCard/modalSlice'
-
-library.add(faGear);
-
-const Setting = () => {
-
-    const dispatch = useDispatch();
-
-    const modalOpenHandler = () => {
-        dispatch(modalActions.openModal())
-    }
-
-    return (
-        <div className="pr-4 w-[50%] h-full flex justify-end items-center">
-            <button onClick={modalOpenHandler} className="bg-palePupple rounded-xl w-full h-4/5 flex items-center justify-center">
-                <FontAwesomeIcon icon={faGear} spin size='2xl'/>
-            </button>
-        </div>
-    )
-}
-
-export default Setting;
+version https://git-lfs.github.com/spec/v1
+oid sha256:8ad0d51f66335220b15fd70f6f8667c10465fdebde3f75a0cef99fb37126d451
+size 813
