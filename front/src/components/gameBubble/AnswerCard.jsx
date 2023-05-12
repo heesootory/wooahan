3f844c4ad823386625692d99b9ac0c59e2721102
Jihoon <kjh8673a@naver.com>
2023-05-12 13:45:48 +0900

    [Feat] 한글방울 맞았습니다 틀렸습니다

--- front/src/components/gameBubble/AnswerCard.jsx
+++ front/src/components/gameBubble/AnswerCard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd5ddbc717aa10fef2a82fe6aa594993ccd079c084b3d5b1bdc32e0b471efcd6
-size 3848
+oid sha256:3f17a01cfe4d84c0a6b1d1c784bbd66d3bc8da41df0a9f5f89d78800db316c1b
+size 3860
