699b0b247cdbac414989c6feedf54bdef762bd97
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-05-13 14:33:51 +0900

    [fix] setting slide error fix

--- front/src/components/main/CardTitle.js
+++ front/src/components/main/CardTitle.js
@@ -1,12 +1,3 @@
-
-
-const CardTitle = (props) => {
-
-    return (
-        <div className="w-32 mt-5 font-['MaplestoryOTFBold'] text-3xl text-white" >
-           {props.title} 
-        </div>
-    )
-}
-
-export default CardTitle;
+version https://git-lfs.github.com/spec/v1
+oid sha256:654ceb3e5abbf3734646f2274dfe56b7db11ec7dce9eb7f5c381d5fc00bf5791
+size 211
