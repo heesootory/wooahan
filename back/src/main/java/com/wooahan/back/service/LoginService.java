91ca5a9d15110ef7becb745a58a8e49d38f4f048
이상민 <lsms3723@daum.net>
2023-05-15 19:57:00 +0900

    [Fix]login logic

--- back/src/main/java/com/wooahan/back/service/LoginService.java
+++ back/src/main/java/com/wooahan/back/service/LoginService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4335965b987dd8d7bda0d4ad83389872350c25e0a13bdccf5c1f965ff533c212
+oid sha256:642f7ef78cc99e43162075183f1226208e6d33db9d71fd8c4af79f9abde301da
 size 5137
