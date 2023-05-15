77ae4dcb678933ffeda61d7566cbd28c289b3981
jiwoo lee <its19447@gmail.com>
2023-05-15 15:34:39 +0900

    [Fix] 백그라운드에서 소리 실행되는것 고치기? ㅈㄱㄴ

--- front/src/util/hooks/useSound.jsx
+++ front/src/util/hooks/useSound.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1276f20d040927049980bf1e2ed23a8b219fb87731500274c44c5e4f1dd85ee2
-size 634
+oid sha256:2002e783edde6a1d7f60785672734a52d851a187b5ec2a3181e0ce82cb789cc8
+size 976
