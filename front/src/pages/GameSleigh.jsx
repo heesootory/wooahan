2087784f10202967a3bd5cc5cb230ecc483e1557
신현광 <rhkd0363@gmail.com>
2023-05-16 23:53:28 +0900

    [Feat] Sleigh 기능 수정

--- front/src/pages/GameSleigh.jsx
+++ front/src/pages/GameSleigh.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09fcea53fc17f0710bc88311bd5cecd06746f8ff73b65ab3cb90dc1d747d9293
-size 15541
+oid sha256:3e04af84c5fa7ba9f44c9da96a66783a76378dc40499085e21b3f367ad4935e6
+size 15559
