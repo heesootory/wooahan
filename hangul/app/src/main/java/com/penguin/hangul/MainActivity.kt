22f43e52c9e78e4271f69fc6d2ced7534349a2dd
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-05-16 20:00:25 +0900

    [feat] google login fix

--- hangul/app/src/main/java/com/penguin/hangul/MainActivity.kt
+++ hangul/app/src/main/java/com/penguin/hangul/MainActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e8e8e0fee1727820309645b51ba8f070f45b24a5a7ff88d3556d72fd7e211f7
-size 5255
+oid sha256:bcb25e53627ce7009dd5eec8d71a84c8b1f1b2c92f6c35e0765c9a7a49f9863b
+size 6889
