ba8e073bf3139d27dc404e00f80576c4d11ac2a1
jihoon_gweon <kjh8673a@naver.com>
2023-05-16 22:18:29 +0900

    [Feat] 게임 시작 전 튜토리얼 추가

--- front/src/components/gameJump/GameJumpOverlay.jsx
+++ front/src/components/gameJump/GameJumpOverlay.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2747e1f7e75e70a78163a9db778a62a90877c0fbb7c9c1c922a1e8c6c4d02334
-size 3167
+oid sha256:9a7525943db0f17467f749803101f3bfc0568ddbf88c36bc882c6bee5508199c
+size 3162
