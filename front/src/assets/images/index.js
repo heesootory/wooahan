564cf5f89b3f91a05602bea3f571e6968c74f65e
jihoon_gweon <kjh8673a@naver.com>
2023-05-18 00:36:51 +0900

    [Feat] 도감 배경 preload 추가

--- front/src/assets/images/index.js
+++ front/src/assets/images/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40c6d0064d3fb10220dcff6f99c20954d6cfbceed28d3e05a17e9454680cc83c
-size 1655
+oid sha256:6d9d042870561a88726265af5555960797df368ab03aeaf85d07599cfdb38ec7
+size 1747
--- front/src/components/gameBubble/QuizCard.jsx
+++ front/src/components/gameBubble/QuizCard.jsx
@@ -1,3 +1,35 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d26b76b68a47621823c9c032b97939bf4b476ff5275b82fd839bd07176aace4c
-size 976
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import BackgroundImageOnLoad from "background-image-on-load";
+
+export const QuizCard = (props) => {
+  const [imageLoadState, setImageLoadState] = useState(false);
+  return (
+    <div
+      className={`${
+        imageLoadState ? "animate-scale-up-center" : "hidden"
+      } absolute w-64 h-64 -mt-32 -ml-32 border-4 border-mainBlack bg-mainWhite rounded-3xl top-1/2 left-1/2`}
+      style={{
+        backgroundImage: `url(${props.image})`,
+        backgroundSize: "16rem 16rem",
+        width: "16rem",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <BackgroundImageOnLoad
+        src={`${props.image}`}
+        onLoadBg={() => setImageLoadState(true)}
+      />
+    </div>
+  );
+};
+
+QuizCard.propTypes = {
+  // second: PropTypes.third
+};
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuizCard);
