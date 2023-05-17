d206d20b1084d697b90b1c995aa14cacc12889b3
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-05-17 14:15:08 +0900

    [feat] location check

--- front/src/components/main/CardImage.js
+++ front/src/components/main/CardImage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8628a7eb0edd63553409d57ef9bd1e4722ed337de8aef7b4d46c87ff102703f0
-size 1597
+oid sha256:3afae9554dd511848ec13bab52f13ad073df9d0ccabe96ecfd925a8431753a99
+size 1531
