874a3930af1fa7bbfe123be7ef7655cd01b2b99d
이상민 <lsms3723@daum.net>
2023-05-17 15:21:20 +0900

    [fix]mp3 length 추가

--- back/src/main/java/com/wooahan/back/controller/LoginController.java
+++ back/src/main/java/com/wooahan/back/controller/LoginController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1270ea123caf01deebf941ddbacde372c3dcbc565cf154461efe8efcc2941721
+oid sha256:f71ffd94b1ccff78036922032858bc6228d420b418919ed4a40c750a6662935c
 size 3966
