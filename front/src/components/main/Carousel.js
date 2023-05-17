07f7d5da0ba0933fe761869ad1387b5672b066da
jihoon_gweon <kjh8673a@naver.com>
2023-05-17 23:18:20 +0900

    [Feat] 메인페이지 캐릭터 동작을 바꿔보았어요

--- front/src/components/main/Carousel.js
+++ front/src/components/main/Carousel.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbba214c11fee6272c15ab9aa61fe5231a4d3db49ba17f38225353d70678ea15
-size 3152
+oid sha256:0f679b131e0fa29303b2e7fe0e0812b9c807bcc00c29f261b5aa0f26a79fce04
+size 3297
