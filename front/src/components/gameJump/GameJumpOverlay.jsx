8ab128283225bdb63a71156911ef7f6618b8a142
jiwoo lee <its19447@gmail.com>
2023-05-17 16:03:28 +0900

    [Add] add for merge amn ㅈㄱㄴ

--- front/src/components/gameJump/GameJumpOverlay.jsx
+++ front/src/components/gameJump/GameJumpOverlay.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eec2b37ff5b0492e6d7c483a64cf496940049e3957e3150780d3f2d3b9394e0f
-size 3154
+oid sha256:d8d1f569478e1d50effa3b8454d2449d320abe4cf20421c1d33ba728b3236ffd
+size 3133
--- front/src/components/main/header/Setting.js
+++ front/src/components/main/header/Setting.js
@@ -1,3 +1,28 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4f796f7bebb455e38f6154e2b00c56c0e1f41fb49ae51837049a914ed412d52a
-size 805
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch } from "react-redux";
+import { modalActions } from "store/features/mainCard/modalSlice";
+
+library.add(faGear);
+
+const Setting = () => {
+  const dispatch = useDispatch();
+
+  const modalOpenHandler = () => {
+    dispatch(modalActions.openModal());
+  };
+
+  return (
+    <div className="pr-4 w-[50%] h-full flex justify-end items-center">
+      <button
+        onClick={modalOpenHandler}
+        className="flex items-center justify-center w-full bg-transparent rounded-xl h-4/5"
+      >
+        <FontAwesomeIcon icon={faGear} size="2xl" />
+      </button>
+    </div>
+  );
+};
+
+export default Setting;
