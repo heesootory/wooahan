cec77eba7f70f7633fd0b3a91e78c77bd587d9d4
jihoon_gweon <kjh8673a@naver.com>
2023-05-17 23:48:45 +0900

    [Feat] 이미지 onload 적용

--- front/src/components/gameBubble/QuizCard.jsx
+++ front/src/components/gameBubble/QuizCard.jsx
@@ -1,27 +1,3 @@
-import React from "react";
-import { connect } from "react-redux";
-
-export const QuizCard = (props) => {
-  return (
-    <div
-      className="absolute w-64 h-64 -mt-32 -ml-32 border-4 border-mainBlack bg-mainWhite rounded-3xl top-1/2 left-1/2"
-      style={{
-        backgroundImage: `url(${props.image})`,
-        backgroundSize: "16rem 16rem",
-        width: "16rem",
-        backgroundRepeat: "no-repeat",
-      }}
-      onClick={props.changeQuiz}
-    ></div>
-  );
-};
-
-QuizCard.propTypes = {
-  // second: PropTypes.third
-};
-
-const mapStateToProps = (state) => ({});
-
-const mapDispatchToProps = {};
-
-export default connect(mapStateToProps, mapDispatchToProps)(QuizCard);
+version https://git-lfs.github.com/spec/v1
+oid sha256:d26b76b68a47621823c9c032b97939bf4b476ff5275b82fd839bd07176aace4c
+size 976
