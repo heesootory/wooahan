9774fe96e95259b49d5755db359e9bd9e8203ac2
jiwoo lee <its19447@gmail.com>
2023-05-17 10:41:08 +0900

    [Merge] commit before merge

--- front/src/pages/GameJump.jsx
+++ front/src/pages/GameJump.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9de425803b4def6781054632961cf021a4996abecd56a15116e89fb234f23c1b
-size 5368
+oid sha256:eefa8047e03dda3a0009176195233d642396398b2b23cb27d1c23dca2e661d12
+size 5614
--- front/src/util/hooks/usePengulJump.jsx
+++ front/src/util/hooks/usePengulJump.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d50e5765fa0401d44c7de1c7eb2c2c1cfca6f70b3023fa2464b341b754235c0
-size 6013
+oid sha256:4ed9e449e029d7eeeba6f8bc7766adb9b7bc402fb2df4c28993900b4c5823283
+size 6155
