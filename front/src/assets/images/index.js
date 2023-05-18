0c51bb506c820f91e9b85f9a6b15cfbe13322830
jiwoo lee <its19447@gmail.com>
2023-05-18 19:46:17 +0900

    [Fix] 버그 수정, gif 학살 야호 맨

--- front/src/assets/images/index.js
+++ front/src/assets/images/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:498196c8ce100c2ac6a04512b20e04e1b6b54b6d6ab4cfa2c7b41beaedadb8c9
-size 1891
+oid sha256:8c885ae592b83676d84751aad8a78193ea011c76c4e3a2725516d3fea82a9912
+size 1897
