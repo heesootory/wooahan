cf1ce391fe479cc99bb8790426cbca16263d1dd9
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-05-18 23:23:38 +0900

    [fix] login fix

--- front/src/pages/Main.jsx
+++ front/src/pages/Main.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e407af5ebe0ba1ffa2b0be8dd16c7a86e6adc2e4da26ee85eac645f04926624e
-size 3046
+oid sha256:d903f4344c009d4c8ecc3db490579fc0d6e9e626fccd2c000e742b2bde1d0906
+size 3060
