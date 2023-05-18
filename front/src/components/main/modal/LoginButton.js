ce5a4a3c67aa221c5acafc4a80a5cc19cb526329
이지우 <its19447@gmail.com>
2023-05-18 12:49:51 +0900

    Update front/src/components/main/modal/LoginButton.js

--- front/src/components/main/modal/LoginButton.js
+++ front/src/components/main/modal/LoginButton.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b157c6a0e05976fd2a6c5e6beb2c1a4854efce83dae9a3c38a4a68c040f8d4cd
-size 2108
+oid sha256:f11ea307404321c97388a2e4da1a16432c7b1a9c5d9339e9280174b225a15250
+size 2105
