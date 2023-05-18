37fc6d003f95b4214253035a8454e3bac304eeec
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-05-18 19:52:48 +0900

    [fix] main fix

--- front/src/components/main/header/Setting.js
+++ front/src/components/main/header/Setting.js
@@ -1,31 +1,3 @@
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGear } from "@fortawesome/free-solid-svg-icons";
-import { useDispatch } from "react-redux";
-import { modalActions } from "store/features/mainCard/modalSlice";
-
-library.add(faGear);
-
-const Setting = (props) => {
-  const dispatch = useDispatch();
-
-  const modalOpenHandler = () => {
-    dispatch(modalActions.openModal());
-  };
-
-  const buttonBackGround = ["bg-buttonPurple", "bg-buttonGreen", "bg-buttonBlue", "bg-buttonRed", "bg-buttonPurple", "bg-buttonGreen", "bg-buttonBlue", "bg-buttonRed"]
-  const pushButton = ["hover:bg-buttonDeepPurple", "hover:bg-buttonDeepGreen", "hover:bg-buttonDeepBlue", "hover:bg-buttonDeepRed", ]
-
-  return (
-    <div className="pr-4 w-[50%] h-full flex justify-end items-center">
-      <button
-        onClick={modalOpenHandler}
-        className={`flex items-center justify-center w-full rounded-xl h-4/5 ${pushButton[props.page]} ${props.page === "books" ? `bg-white` : buttonBackGround[props.page]}`}
-      >
-        <FontAwesomeIcon icon={faGear} size="2xl" />
-      </button>
-    </div>
-  );
-};
-
-export default Setting;
+version https://git-lfs.github.com/spec/v1
+oid sha256:b176e7420384661d90a90815a44df5b910c33d5a81f968e9f684d46e4d09f96a
+size 1196
--- front/src/pages/Main.jsx
+++ front/src/pages/Main.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f46b4d37e521f9148c1a8896ea2e1dbb418e520357c9095bf0ae3737d9c1bc85
-size 3352
+oid sha256:7c6c177bc0cc28769d72cdb727e384c505fee1f8f486823c31147416ece38565
+size 3298
