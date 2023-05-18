453b1becf968c77a03eb5644e4c949763b3d476d
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-05-18 19:24:53 +0900

    [fix] login button fix

--- front/src/components/main/modal/KakaoLogin.js
+++ front/src/components/main/modal/KakaoLogin.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78e3192cb3b1bf20f9fcb6889d9cafc5cbebeea09932d5439f80b2d4f34c603d
-size 1131
+oid sha256:3373ad92b8809847a453ba55c22167e1322fe49982e1ef72e17222ec439cac61
+size 1092
