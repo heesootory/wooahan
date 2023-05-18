931ec0e8466777e65f378b6f4aedca85876061a5
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-05-18 23:19:11 +0900

    [fix] deviceId

--- front/src/pages/Main.jsx
+++ front/src/pages/Main.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd9bcf92ff6e9abd6a75ff789bf6ae8fe8640500fe629ea355434dfe377366ad
-size 3127
+oid sha256:e407af5ebe0ba1ffa2b0be8dd16c7a86e6adc2e4da26ee85eac645f04926624e
+size 3046
