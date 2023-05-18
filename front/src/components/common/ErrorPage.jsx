e933c6fdfe1f046d81468b99bf169ffbda7e7513
jihoon_gweon <kjh8673a@naver.com>
2023-05-18 19:04:45 +0900

    [Feat] 에러페이지, 카카오버튼 디자인 수정

--- front/src/components/common/ErrorPage.jsx
+++ front/src/components/common/ErrorPage.jsx
@@ -1,21 +1,3 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-
-const ErrorPage = () => {
-  const navigation = useNavigate();
-  return (
-    <div className="w-screen h-screen absolute top-0 text-center justify-center items-center">
-      <div className="font-MaplestoryBold text-[4vw]">요청에 실패했습니다.</div>
-      <button
-        className="font-MaplestoryBold text-[2vw] border-2"
-        onClick={() => {
-          navigation("/");
-        }}
-      >
-        홈으로
-      </button>
-    </div>
-  );
-};
-
-export default ErrorPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:1ee7d28b4921a04bf50b9b144a7e4c53ba1c302239c237d53ee4939b3058b20b
+size 1068
--- front/src/components/main/modal/KakaoLogin.js
+++ front/src/components/main/modal/KakaoLogin.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78e3192cb3b1bf20f9fcb6889d9cafc5cbebeea09932d5439f80b2d4f34c603d
-size 1131
+oid sha256:122f08325b41b9026b8c8182cddd347a3345386b49664c80c8ac46977c0836fc
+size 1144
