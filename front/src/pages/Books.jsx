3fe3a0b2580da4bff3a15ec3505618e9ebf10323
jihoon_gweon <kjh8673a@naver.com>
2023-05-18 21:04:26 +0900

    [Feat] 도감페이지 소리 설정

--- front/src/pages/Books.jsx
+++ front/src/pages/Books.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65c8576a39686d924486e56b0718ab7b11bf25a6dbfb810ff0ba1df6e76c04bd
-size 1937
+oid sha256:5f25b5f0c865b399f868efbc0dfb0f826eeae9811cf355925f9aa3eea4eadb64
+size 2084
