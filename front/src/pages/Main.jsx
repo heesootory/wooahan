279ef4da2244104221eac439f2f140be9e976faa
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-05-18 13:18:16 +0900

    [feat] ui button

--- front/src/pages/Main.jsx
+++ front/src/pages/Main.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b38192e21ace49c2476c525c121017884f5d8a3f336261492926fd4dd6b0ee62
-size 2883
+oid sha256:aecb58b84c4ec3c2fab45a33f07ef772453d284d2a0fbbd26d19ac90c38f6a60
+size 2895
