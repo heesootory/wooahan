5763bb960ead8cd701939ea04ff3407777c98eb4
신현광 <rhkd0363@gmail.com>
2023-05-19 10:46:08 +0900

    [Feat] Many Update

--- front/src/pages/GameSleigh.jsx
+++ front/src/pages/GameSleigh.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bad040adb9ba63f97220750bbc7f1f741eaa3e8801d5e83768db770959149f65
-size 15950
+oid sha256:1cd4bb6ee1fded49a0c718a3f1eb9ef6faffdd6f0d1213f8ba31e77a7e324b8f
+size 13956
