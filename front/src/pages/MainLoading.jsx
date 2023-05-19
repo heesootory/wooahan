8874c0fecc9e0e3dd0ef73b559eecd57c803bbbc
신현광 <rhkd0363@gmail.com>
2023-05-19 11:05:59 +0900

    [Fix] loginInfo

--- front/src/pages/MainLoading.jsx
+++ front/src/pages/MainLoading.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6803a0500437a9cf8fd4d2f747983235c8a228176b4ed148ea274835d41dd32a
-size 1652
+oid sha256:c431cd5fbb0fb974ed4f63dcba75f25d25b494d46edfd31d416d4a26be289e06
+size 1649
