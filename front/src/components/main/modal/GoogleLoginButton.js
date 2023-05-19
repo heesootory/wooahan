c430d0c30122662db5d16628b0d88b4d16a9bd41
신현광 <rhkd0363@gmail.com>
2023-05-19 09:37:01 +0900

    [Style] 로그인 성공페이지 수정

--- front/src/components/main/modal/GoogleLoginButton.js
+++ front/src/components/main/modal/GoogleLoginButton.js
@@ -1,26 +1,3 @@
-
-import {GoogleLogin} from "@react-oauth/google";
-import {GoogleOAuthProvider} from "@react-oauth/google";
-
-
-const url = "https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id=658207955186-n84qpvfhtdi82n6mfvbmh6v99aevulv7.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Fk8b206.p.ssafy.io%2Fapi%2Flogin%2Foauth2%2Fcode%2Fgoogle&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&service=lso&o2v=1&flowName=GeneralOAuthFlow"
-
-const GoogleLoginButton = () => {
-    const clientId = 'clientID'
-    return (
-        <>
-            <GoogleOAuthProvider clientId={clientId}>
-                <GoogleLogin
-                    onSuccess={(res) => {
-                        console.log(res);
-                    }}
-                    onFailure={(err) => {
-                        console.log(err);
-                    }}
-                />
-            </GoogleOAuthProvider>
-        </>
-    );
-};
-
-export default GoogleLoginButton
+version https://git-lfs.github.com/spec/v1
+oid sha256:e38290784c3ff5181f3a21eee87b754401210a88a50c847f028d9ab185f91d35
+size 1035
--- front/src/components/main/modal/Login.js
+++ front/src/components/main/modal/Login.js
@@ -1,16 +1,3 @@
-import GoogleLogin from "./GoogleLogin";
-import GoogleLoginButton from "./GoogleLoginButton";
-import KakaoLogin from "./KakaoLogin";
-
-const Login = () => {
-  return (
-    <div className="h-[40%] mt-[3%]">
-      <div className="font-['MaplestoryOTFBold'] text-[5vh] ">소셜 로그인</div>
-      <KakaoLogin />
-      <GoogleLogin />
-      {/* <GoogleLoginButton /> */}
-    </div>
-  );
-};
-
-export default Login;
+version https://git-lfs.github.com/spec/v1
+oid sha256:652d06f87d761d217f35602e35c76acae7704df56e290a64b0d5a3270c2bed76
+size 411
